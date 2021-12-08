c176fe0396c0619a9537f7e20b980498b621a88c
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2021-12-08 07:05:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import data from './data'
 import List from './List'
 function App() {
   const [people, setPeople] = useState(data)
+ 
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
+        <h3>{people.length} Persons Profile</h3>
+        <div className="sub-class">
+          <List people={people}/>
+          <button onClick={() => setPeople([])}>clear all</button>
+        </div>
       </section>
     </main>
   )
